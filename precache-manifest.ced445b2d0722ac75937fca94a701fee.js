9cd1991f4df059cbad7324400ddad65830c8d11c
Tom郭俊杰 <1501832474@qq.com>
2018-12-14 21:35:31 +0800

    V1 2018-12-14 build 1

---
+++ precache-manifest.ced445b2d0722ac75937fca94a701fee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "05a022318e5e1e5ae1f5",
+    "url": "/static/js/main.05a02231.chunk.js"
+  },
+  {
+    "revision": "62a23992f72e2bc22e9a",
+    "url": "/static/js/1.62a23992.chunk.js"
+  },
+  {
+    "revision": "05a022318e5e1e5ae1f5",
+    "url": "/static/css/main.29c95e55.chunk.css"
+  },
+  {
+    "revision": "62a23992f72e2bc22e9a",
+    "url": "/static/css/1.6c94afb5.chunk.css"
+  },
+  {
+    "revision": "5ebbf642cca01ed5814cbddd45a9aef4",
+    "url": "/index.html"
+  }
+];
